a47e1339e57844ff7f56625850b15ead8d00d58c
Royce Remulla <royce.com@gmail.com>
2016-09-12 14:13:05 +0800

    Added route for comment

--- learn-rails/blog/config/routes.rb
+++ learn-rails/blog/config/routes.rb
@@ -1,9 +1,11 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   root 'welcome#index'
 end
