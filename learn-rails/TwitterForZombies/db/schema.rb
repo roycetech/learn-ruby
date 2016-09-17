fd43290b78e98d76440f0c5d811f0a5698306f35
Royce Remulla <royce.com@gmail.com>
2016-09-17 14:55:10 +0800

    Updated schema by the migration

--- learn-rails/TwitterForZombies/db/schema.rb
+++ learn-rails/TwitterForZombies/db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160917062241) do
+ActiveRecord::Schema.define(version: 20160917064112) do
 
   create_table "zombies", force: :cascade do |t|
     t.string   "name"
     t.text     "bio"
     t.integer  "age"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.string   "email"
+    t.boolean  "rotting",    default: false
   end
 
 end
